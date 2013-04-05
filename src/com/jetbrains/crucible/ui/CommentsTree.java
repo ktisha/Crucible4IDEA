9faff013fad902643d2baef43caa64c0831a6df1
ktisha <kt@jetbrains.com>
2013-04-05 19:39:30 +0400

    reply to versioned comment action added

--- src/com/jetbrains/crucible/ui/CommentsTree.java
+++ src/com/jetbrains/crucible/ui/CommentsTree.java
@@ -3,18 +3,18 @@ package com.jetbrains.crucible.ui;
 import com.intellij.ide.util.treeView.AbstractTreeBuilder;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.PopupHandler;
 import com.intellij.ui.treeStructure.SimpleNode;
+import com.intellij.ui.treeStructure.SimpleTree;
 import com.intellij.ui.treeStructure.SimpleTreeStructure;
 import com.jetbrains.crucible.actions.ReplyToCommentAction;
 import com.jetbrains.crucible.model.Comment;
 import com.jetbrains.crucible.model.Review;
 import com.jetbrains.crucible.ui.toolWindow.CrucibleTreeStructure;
 
-import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,25 +22,24 @@ import java.util.List;
 /**
  * User: ktisha
  */
-public class ReviewForm extends JTree {
+public class CommentsTree extends SimpleTree {
 
   private static final int ourBalloonWidth = 400;
   private static final int ourBalloonHeight = 400;
 
-  public ReviewForm(Review review, final Comment comment, final Project project, boolean editable) {
+  public CommentsTree(Review review, final Comment comment, final Project project, Editor editor, VirtualFile vFile) {
     final CommentNode root = new CommentNode(comment);
     SimpleTreeStructure structure = new CrucibleTreeStructure(project, root);
-    final DefaultTreeModel model = new DefaultTreeModel(new DefaultMutableTreeNode());
     AbstractTreeBuilder reviewTreeBuilder =
-      new AbstractTreeBuilder(this, model, structure, null);
+      new AbstractTreeBuilder(this, getBuilderModel(), structure, null);
     invalidate();
 
     setPreferredSize(new Dimension(ourBalloonWidth, ourBalloonHeight));
 
     DefaultActionGroup group = new DefaultActionGroup();
-    final ReplyToCommentAction addCommentAction = new ReplyToCommentAction(review, "Add Reply", "Comment");
-    addCommentAction.setContextComponent(this);
-    group.add(addCommentAction);
+    final ReplyToCommentAction replyToComment = new ReplyToCommentAction(review, editor, vFile, "Add Reply", "Comment");
+    replyToComment.setContextComponent(this);
+    group.add(replyToComment);
     PopupHandler.installUnknownPopupHandler(this, group, ActionManager.getInstance());
   }
 
