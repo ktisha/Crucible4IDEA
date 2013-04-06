1302cbfec70b454faa4f2e93bcd018c1b548749a
ktisha <kt@jetbrains.com>
2013-04-07 00:38:38 +0400

    moved general comments from table to tree

---
+++ src/com/jetbrains/crucible/ui/toolWindow/CommentNode.java
@@ -0,0 +1,37 @@
+package com.jetbrains.crucible.ui.toolWindow;
+
+import com.intellij.ui.treeStructure.SimpleNode;
+import com.jetbrains.crucible.model.Comment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: ktisha
+ */
+public class CommentNode extends SimpleNode {
+  private final Comment myComment;
+
+  public CommentNode(Comment comment) {
+    myComment = comment;
+  }
+
+  public Comment getComment() {
+    return myComment;
+  }
+
+  @Override
+  public String getName() {
+    return myComment.getAuthor() + " : " + myComment.getMessage();
+  }
+
+  @Override
+  public SimpleNode[] getChildren() {
+    final List<Comment> replies = myComment.getReplies();
+    final List<SimpleNode> children = new ArrayList<SimpleNode>();
+    for (Comment reply : replies) {
+      children.add(new CommentNode(reply));
+    }
+    return children.toArray(new SimpleNode[children.size()]);
+  }
+}
