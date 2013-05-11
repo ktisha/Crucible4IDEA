3fc93c933526622dd6737f6405d340b7fcf3e7f5
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-05-11 20:29:51 +0400

    improved comments tree UI a bit

--- src/com/jetbrains/crucible/ui/toolWindow/details/CommentsTree.java
+++ src/com/jetbrains/crucible/ui/toolWindow/details/CommentsTree.java
@@ -5,9 +5,12 @@ import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.JBDefaultTreeCellRenderer;
 import com.intellij.ui.PopupHandler;
 import com.intellij.ui.treeStructure.SimpleTree;
 import com.intellij.ui.treeStructure.SimpleTreeStructure;
+import com.intellij.util.ui.UIUtil;
+import com.intellij.util.ui.tree.TreeUtil;
 import com.jetbrains.crucible.actions.AddCommentAction;
 import com.jetbrains.crucible.model.Comment;
 import com.jetbrains.crucible.model.Review;
@@ -16,22 +19,24 @@ import com.jetbrains.crucible.utils.CrucibleBundle;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 /**
  * User: ktisha
  */
 public class CommentsTree extends SimpleTree {
 
-  private static final int ourBalloonWidth = 450;
-  private static final int ourBalloonHeight = 400;
+  private static int ourBalloonWidth = 550;
+  private static int ourBalloonHeight = 200;
 
   public CommentsTree(@NotNull final Review review, @NotNull final Comment comment,
                       @NotNull final Editor editor, @NotNull final VirtualFile vFile) {
     final CommentNode root = new CommentNode(comment);
     setExpandableItemsEnabled(false);
-    setCellRenderer(new DefaultTreeCellRenderer() {
+
+    setCellRenderer(new JBDefaultTreeCellRenderer(this) {
       @Override
       public Component getTreeCellRendererComponent(JTree tree,
                                                     Object value,
@@ -42,8 +47,38 @@ public class CommentsTree extends SimpleTree {
                                                     boolean hasFocus) {
         final JLabel label = (JLabel)super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
         label.setToolTipText(label.getText());
+        label.setText("<html>" + label.getText() + "</html>");
         setIcon(null);
+        String[] lines = UIUtil.splitText(label.getText(), getFontMetrics(UIUtil.getLabelFont()), ourBalloonWidth, ',');
+        Font f = label.getFont();
+        FontMetrics metrics = label.getFontMetrics(f);
+        int hgt = (metrics.getHeight() + 10) * lines.length;
+        Dimension size = new Dimension(ourBalloonWidth - 50, hgt);
+        label.setPreferredSize(size);
+        label.setBorder(BorderFactory.createEtchedBorder());
+        label.setVerticalAlignment(TOP);
         return label;
+      }
+    });
+    addComponentListener(new ComponentListener() {
+      @Override
+      public void componentResized(ComponentEvent e) {
+        ourBalloonHeight = e.getComponent().getHeight();
+        ourBalloonWidth = e.getComponent().getWidth();
+        TreeUtil.collapseAll(CommentsTree.this, -1);
+        TreeUtil.expandAll(CommentsTree.this);
+      }
+
+      @Override
+      public void componentMoved(ComponentEvent e) {
+      }
+
+      @Override
+      public void componentShown(ComponentEvent e) {
+      }
+
+      @Override
+      public void componentHidden(ComponentEvent e) {
       }
     });
     final SimpleTreeStructure structure = new CrucibleTreeStructure(root);
@@ -58,5 +93,6 @@ public class CommentsTree extends SimpleTree {
     replyToComment.setContextComponent(this);
     group.add(replyToComment);
     PopupHandler.installUnknownPopupHandler(this, group, ActionManager.getInstance());
+    TreeUtil.expandAll(this);
   }
 }
